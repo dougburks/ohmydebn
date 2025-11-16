264813eeb778420b0cb334dd53d530cfb42b7c07
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-16 15:00:49 -0500

    update scripts to work before packages are installed

--- install/preflight/os.sh
+++ install/preflight/os.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if ! grep -q "13 (trixie)" /etc/os-release; then
-  /opt/ohmydebn/bin/ohmydebn-headline "cat" "OhMyDebn is designed for Debian 13 Cinnamon. Exiting!"
+  echo "OhMyDebn is designed for Debian 13 Cinnamon. Exiting!"
   exit 1
 fi
--- install/preflight/user.sh
+++ install/preflight/user.sh
@@ -2,13 +2,15 @@
 
 if [ "$UID" -eq 0 ]; then
 
-  /opt/ohmydebn/bin/ohmydebn-headline "cat" "Looks like you're running as root.
+  cat <<EOF
+Looks like you're running as root.
  
 Instead of running as root, you most likely want to 
 run this as a normal user that has sudo privileges.
 
 Press Enter if you are sure you want to continue as root
-or Ctrl-c to cancel."
+or Ctrl-c to cancel.
+EOF
 
   read input
 fi

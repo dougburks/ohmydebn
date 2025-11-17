172d382bb0c5872bcfbe5d5034f29a220b097b08
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-17 16:17:23 -0500

    improve install.sh

--- install.sh
+++ install.sh
@@ -58,41 +58,42 @@ WARNING!
 Press Enter to continue or Ctrl-c to cancel.
 EOF
   read input
-fi
 
-# Check to see if we have an APT configuration
-if [ -f /etc/apt/sources.list.d/debian.sources ] || [ -f /etc/apt/sources.list.d/proxmox.sources ]; then
-  echo "Found an APT sources file in /etc/apt/sources.list.d/"
-else
-  # Some Debian installation methods have a broken APT configuration so try to work around that
-  SOURCESLIST=/etc/apt/sources.list
-  if ! grep -q "debian.org" $SOURCESLIST >/dev/null 2>&1; then
-    echo "$SOURCESLIST does not have any debian.org references."
-    if [ -f $SOURCESLIST ]; then
-      echo "Renaming $SOURCESLIST to $SOURCESLIST.orig"
-      sudo mv $SOURCESLIST $SOURCESLIST.orig
-    fi
-    DEBIANSOURCES=/etc/apt/sources.list.d/debian.sources
-    if [ ! -f $DEBIANSOURCES ]; then
-      echo "Creating $DEBIANSOURCES and adding the following:"
-      cat <<EOF | sudo tee -a $DEBIANSOURCES
-Types: deb
-URIs: https://deb.debian.org/debian
-Suites: trixie trixie-updates
-Components: main non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+  # Check to see if we have an APT configuration
+  if [ -f /etc/apt/sources.list.d/debian.sources ] || [ -f /etc/apt/sources.list.d/proxmox.sources ]; then
+    echo "Found an APT sources file in /etc/apt/sources.list.d/"
+  else
+    # Some Debian installation methods have a broken APT configuration so try to work around that
+    SOURCESLIST=/etc/apt/sources.list
+    if ! grep -q "debian.org" $SOURCESLIST >/dev/null 2>&1; then
+      echo "$SOURCESLIST does not have any debian.org references."
+      if [ -f $SOURCESLIST ]; then
+        echo "Renaming $SOURCESLIST to $SOURCESLIST.orig"
+        sudo mv $SOURCESLIST $SOURCESLIST.orig
+      fi
+      DEBIANSOURCES=/etc/apt/sources.list.d/debian.sources
+      if [ ! -f $DEBIANSOURCES ]; then
+        echo "Creating $DEBIANSOURCES and adding the following:"
+        cat <<EOF | sudo tee -a $DEBIANSOURCES
+  Types: deb
+  URIs: https://deb.debian.org/debian
+  Suites: trixie trixie-updates
+  Components: main non-free-firmware
+  Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-Types: deb
-URIs: https://security.debian.org/debian-security
-Suites: trixie-security
-Components: main non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-EOF
+  Types: deb
+  URIs: https://security.debian.org/debian-security
+  Suites: trixie-security
+  Components: main non-free-firmware
+  Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+  EOF
+      fi
     fi
   fi
-fi
 
-sudo apt update && sudo apt install -y curl gpg
+  sudo apt update && sudo apt install -y curl gpg
+
+fi
 
 if [ ! -f /etc/apt/sources.list.d/ohmydebn.sources ]; then
   sudo tee /etc/apt/sources.list.d/ohmydebn.sources <<EOF

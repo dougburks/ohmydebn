d01413be25646d0faa2c07595c216ee77f3734c8
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-17 15:56:13 -0500

    fix mkdir

--- install/config/caskaydia.sh
+++ install/config/caskaydia.sh
@@ -3,7 +3,7 @@
 if [ ! -f ~/.local/share/fonts/CaskaydiaMonoNerdFont-Regular.ttf ]; then
   /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Configuring Caskaydia Nerd Font"
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CascadiaMono.zip
-  mkdir -p ~/.local/share
+  mkdir -p ~/.local/share/fonts
   unzip CascadiaMono.zip -d ~/.local/share/fonts
   rm -f CascadiaMono.zip
   fc-cache -fv

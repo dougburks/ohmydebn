8905ef8f59697b9eef1ddff23eb902a9c793422c
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-17 13:28:21 -0500

    stop manually installing mint packages

--- install/packaging/cinnamon.sh
+++ install/packaging/cinnamon.sh
@@ -16,29 +16,4 @@ if [ ! -f ~/.local/state/ohmydebn ]; then
 
   /usr/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Configuring base OS"
 
-  if [ $(dpkg -l | grep "^ii  mint-" | wc -l) -eq 0 ]; then
-    /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Downloading Cinnamon themes"
-    ARCH=$(uname -m)
-    if [ "$ARCH" != "x86_64" ]; then
-      sudo dpkg --add-architecture amd64
-    fi
-    MINTLIST="/etc/apt/sources.list.d/mint.list"
-    MINTKEY="linuxmint-keyring_2022.06.21_all.deb"
-    MINTURL="http://packages.linuxmint.com/pool/main/l/linuxmint-keyring/$MINTKEY"
-    echo
-    echo "Temporarily adding the following repo:"
-    echo
-    echo "deb [arch=amd64] http://packages.linuxmint.com virginia main" | sudo tee -a $MINTLIST
-    curl -O $MINTURL
-    echo
-    sudo dpkg -i $MINTKEY
-    echo
-    sudo rm -f $MINTKEY
-    sudo apt update && sudo apt -y install mint-themes mint-x-icons mint-y-icons
-    echo
-    sudo rm -f $MINTLIST
-    sudo apt update
-    echo
-    sudo apt -y purge linuxmint-keyring
-  fi
 fi

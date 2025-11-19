d263f80c0629c3ef6adece365b3856cd5c7b8719
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-19 08:26:07 -0500

    improve update commands

--- install/finalization/updates.sh
+++ install/finalization/updates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Installing any available package updates"
-sudo apt update && apt -y full-upgrade
+sudo apt update && sudo apt -y full-upgrade

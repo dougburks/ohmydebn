3382193e442130ab9e5e29dd1ae2c67f0b803119
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-28 07:15:54 -0500

    improve cleanup/fonts.sh

--- install/cleanup/fonts.sh
+++ install/cleanup/fonts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [ -f ~/.local/share/fonts/CaskaydiaMonoNerdFont-Regular.ttf ]; then
-  /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Cleaning up old fonts"
-  rm -f ~/.local/share/fonts/CaskaydiaMonoNerdFont*
+DIR=~/.local/share/fonts
+if [ -f $DIR/CaskaydiaMonoNerdFont-Regular.ttf ]; then
+  /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Cleaning up $DIR"
+  rm -f $DIR/CaskaydiaMonoNerdFont*
+  rm -f $DIR/LICENSE
+  rm -f $DIR/README.md
 fi

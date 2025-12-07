d264a70da93ac01cd1e1bde70b0832a83affb9c7
Doug Burks <doug.burks@securityonionsolutions.com>
2025-12-07 17:06:57 -0500

    fix syntax

--- install/keybinding/keybinding.sh
+++ install/keybinding/keybinding.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STATE_DIR=~/.local/state/ohmydebn-config
-KEYBINDING_STATE=$STATE_DIR/keybinding-20251130
+KEYBINDING_STATE=$STATE_DIR/keybinding-20251207
 
 if [ ! -f $KEYBINDING_STATE ]; then
   /usr/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Updating hotkeys"

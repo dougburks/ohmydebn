63fb7358770b7d9b14d75ec769383a9c51b7b5e9
Doug Burks <doug.burks@securityonionsolutions.com>
2026-02-13 14:05:33 -0500

    improve ohmydebn-theme-set and bump version numbers

--- install/finalization/updates.sh
+++ install/finalization/updates.sh
@@ -3,4 +3,4 @@
 /usr/share/ohmydebn/bin/ohmydebn-headline "tte rain" "Installing any available package updates"
 sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt -y full-upgrade
 echo
-/usr/share/ohmydebn/bin/ohmydebn-update-opencode 1.1.41
+/usr/share/ohmydebn/bin/ohmydebn-update-opencode 1.1.65

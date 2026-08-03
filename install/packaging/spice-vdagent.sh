4f42eee64ea1c9faba0784753632e38c0d567b2f
Doug Burks <doug.burks@securityonionsolutions.com>
2026-08-03 11:24:52 -0400

    4.1.0

--- install/packaging/spice-vdagent.sh
+++ install/packaging/spice-vdagent.sh
@@ -9,5 +9,6 @@ if [ ! -f $SPICE_VDAGENT_STATE ]; then
   # We need to force them to downgrade to the working version.
   sudo /usr/bin/apt -y install spice-vdagent=0.22.1-4.1 --allow-downgrades
 
+  mkdir -p ~/.local/state/ohmydebn-config
   touch $SPICE_VDAGENT_STATE
 fi

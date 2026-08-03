e12c1f09e20b58294d6fb8d6df16d9229f8c68d3
Doug Burks <doug.burks@securityonionsolutions.com>
2026-08-03 11:06:25 -0400

    4.1.0

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# install
 source $OHMYDEBN_INSTALL/packaging/cinnamon.sh
 source $OHMYDEBN_INSTALL/packaging/dbus.sh
+source $OHMYDEBN_INSTALL/packaging/spice-vdagent.sh
+
+# remove
 source $OHMYDEBN_INSTALL/packaging/remove.sh
---
+++ install/packaging/spice-vdagent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SPICE_VDAGENT_STATE=~/.local/state/ohmydebn-config/spice-vdagent-20260803
+if [ ! -f $SPICE_VDAGENT_STATE ]; then
+
+  /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Making sure that the correct version of spice-vdagent is installed"
+  # Debian Forky (and its derivates like Kali) includes a newer version of spice-vdagent.
+  # This newer version has a regression where it won't resize the screen properly.
+  # We need to force them to downgrade to the working version.
+  sudo /usr/bin/apt -y install spice-vdagent=0.22.1-4.1 --allow-downgrades
+
+  touch $SPICE_VDAGENT_STATE
+fi

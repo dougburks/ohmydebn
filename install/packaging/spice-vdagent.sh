b38bf0c77581003f0dff830bd347ac1bda0783aa
Doug Burks <doug.burks@securityonionsolutions.com>
2026-08-03 13:32:19 -0400

    4.1.0

--- install/packaging/spice-vdagent.sh
+++ install/packaging/spice-vdagent.sh
@@ -4,11 +4,24 @@ SPICE_VDAGENT_STATE=~/.local/state/ohmydebn-config/spice-vdagent-20260803
 if [ ! -f $SPICE_VDAGENT_STATE ]; then
 
   /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Making sure that the correct version of spice-vdagent is installed"
+
   # Debian Forky (and its derivates like Kali) includes a newer version of spice-vdagent.
   # This newer version has a regression where it won't resize the screen properly.
   # We need to force them to downgrade to the working version.
   sudo /usr/bin/apt -y install spice-vdagent=0.22.1-4.1 --allow-downgrades
 
+  # Next we need to pin the version so that it can't be updated later
+  sudo mkdir -p /etc/apt/preferences.d/
+  sudo tee /etc/apt/preferences.d/ohmydebn-spice-vdagent >/dev/null <<'EOF'
+Package: spice-vdagent
+Pin: version 0.22.1-4.1
+Pin-Priority: 1001
+EOF
+
+  # Set permissions on the file
+  sudo chmod 644 /etc/apt/preferences.d/ohmydebn-spice-vdagent
+
+  # Finally, touch the state file so that we don't run this code on every update.
   mkdir -p ~/.local/state/ohmydebn-config
   touch $SPICE_VDAGENT_STATE
 fi
